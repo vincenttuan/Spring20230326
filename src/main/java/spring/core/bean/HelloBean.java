8358fc03ed20e1e9ad151de60d69d344b5a2a401
vince <vince@DESKTOP-GVCMMLC>
2023-03-26 15:51:31 +0800

    web

---
+++ src/main/java/spring/core/bean/HelloBean.java
@@ -0,0 +1,14 @@
+package spring.core.bean;
+
+public class HelloBean {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
