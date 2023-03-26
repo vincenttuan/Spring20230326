797882aba85a14f532fba757d8ae797d2c9929fc
vince <vince@DESKTOP-GVCMMLC>
2023-03-26 16:58:46 +0800

    web

--- src/main/java/spring/core/bean/TestCalc.java
+++ src/main/java/spring/core/bean/TestCalc.java
@@ -10,12 +10,14 @@ public class TestCalc {
 		//Calc calc = ctx.getBean(Calc.class);
 		Calc calc = ctx.getBean("calc", Calc.class);
 		calc.add(10, 20);
-		System.out.println(calc.getResult());
+		System.out.println(calc.getResult()); // 30
 		
 		Calc calc2 = ctx.getBean("calc", Calc.class);
 		calc2.add(30, 40);
-		System.out.println(calc2.getResult());
-
+		System.out.println(calc2.getResult()); // 70
+		
+		
+		System.out.println(calc.getResult()); // ??
 	}
 
 }
