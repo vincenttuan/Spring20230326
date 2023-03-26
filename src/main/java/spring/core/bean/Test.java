a69a4894bf29bb9ccd2c700db3f876acc7241d89
vince <vince@DESKTOP-GVCMMLC>
2023-03-26 16:06:43 +0800

    web

--- src/main/java/spring/core/bean/Test.java
+++ src/main/java/spring/core/bean/Test.java
@@ -13,6 +13,7 @@ public class Test {
 		// 使用 Spring IOC 反轉控制技術
 		// 1. 找到配置檔
 		ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/java/spring/core/bean/beans-config.xml");
+		// 2. 取得物件
 		HelloBean helloBean1 = (HelloBean)ctx.getBean("helloBean1");
 		helloBean1.setName("Mary");
 		System.out.println(helloBean1.getName());
