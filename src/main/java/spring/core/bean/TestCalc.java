a5b6f60e2665c52c24f916502129222ffc3af54f
vince <vince@DESKTOP-GVCMMLC>
2023-03-26 16:45:16 +0800

    web

--- src/main/java/spring/core/bean/TestCalc.java
+++ src/main/java/spring/core/bean/TestCalc.java
@@ -1,9 +1,17 @@
 package spring.core.bean;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class TestCalc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/java/spring/core/bean/beans-config.xml");
+		//Calc calc = ctx.getBean(Calc.class);
+		Calc calc = ctx.getBean("calc", Calc.class);
+		calc.add(10, 20);
+		
+		System.out.println(calc.getResult());
 
 	}
 
