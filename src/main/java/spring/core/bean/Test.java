a47408426b82f609f0be50862bcb5e353e81cded
vince <vince@DESKTOP-GVCMMLC>
2023-03-26 15:53:42 +0800

    web

---
+++ src/main/java/spring/core/bean/Test.java
@@ -0,0 +1,12 @@
+package spring.core.bean;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HelloBean helloBean = new HelloBean();
+		helloBean.setName("Spring");
+		System.out.println(helloBean.getName());
+
+	}
+
+}
