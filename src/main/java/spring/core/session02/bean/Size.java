1c30c74a533c8a16f517aa85c0832370e6572605
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-09 16:14:50 +0800

    spring

--- src/main/java/spring/core/session02/bean/Size.java
+++ src/main/java/spring/core/session02/bean/Size.java
@@ -1,5 +1,18 @@
 package spring.core.session02.bean;
 
 public class Size {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Color [name=" + name + "]";
+	}
 }
