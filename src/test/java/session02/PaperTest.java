d2feac915f1b1d0aecbd935c010725cc5ac2c774
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-09 16:24:34 +0800

    spring

---
+++ src/test/java/session02/PaperTest.java
@@ -0,0 +1,14 @@
+package session02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session02.bean.Paper;
+
+public class PaperTest {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config2.xml");
+		Paper paper1 = ctx.getBean("paper1", Paper.class);
+		System.out.println(paper1);
+	}
+}
