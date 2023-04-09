2845c6a764c9a99cd24c5cef3c2ad31578837d63
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-09 14:56:33 +0800

    web

---
+++ src/test/java/session02/AuthorTest.java
@@ -0,0 +1,30 @@
+package session02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session02.bean.Author;
+
+public class AuthorTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config2.xml");
+		
+		Author author1 = ctx.getBean("author1", Author.class);
+		System.out.println("author1: " + author1); 
+		// 要透過手動自行注入資料
+		author1.setName("Bob");
+		author1.setSex('男');
+		author1.setAge(24);
+		System.out.println("author1: " + author1); 
+		
+		Author author2 = ctx.getBean("author2", Author.class);
+		System.out.println("author2: " + author2); 
+		
+		Author author3 = ctx.getBean("author3", Author.class);
+		System.out.println("author3: " + author3); 
+		
+
+	}
+
+}
