67705840a22ed1b8ee618f9cbe7a4fccde7013ec
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 16:45:09 +0800

    spring

--- src/test/java/session03/StudentTest.java
+++ src/test/java/session03/StudentTest.java
@@ -14,7 +14,9 @@ public class StudentTest {
 		List<Student> students = ctx.getBean("students", List.class);
 		System.out.println(students);
 		// 請印出每一個學生各修了多少學分 ?
-		
+		// 例如：
+		// John: 6
+		// Mary: 4
 	}
 
 }
