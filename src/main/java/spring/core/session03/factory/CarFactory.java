5722270df7c5ee1c434be31b8c7a91c7232b32a5
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 16:19:10 +0800

    spring

---
+++ src/main/java/spring/core/session03/factory/CarFactory.java
@@ -0,0 +1,28 @@
+package spring.core.session03.factory;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.FactoryBean;
+
+import spring.core.session03.bean.Car;
+
+public class CarFactory implements FactoryBean<Car> {
+
+	@Override
+	public Car getObject() throws Exception {
+		Random random = new Random();
+		int price = random.nextInt(300_0000);
+		return new Car("BMW", price);
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Car.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+}
