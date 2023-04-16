c1cbb7b221e22157b2254e713c56670604d01212
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 13:31:02 +0800

    spring

---
+++ src/test/java/session03/ClazzTest.java
@@ -0,0 +1,16 @@
+package session03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session03.bean.Clazz;
+
+public class ClazzTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		Clazz clazz1 = ctx.getBean("clazz1", Clazz.class);
+		System.out.println(clazz1);
+	}
+
+}
