07488f467204ca8031aaf3dd757fde9fd20cde8e
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 14:24:47 +0800

    spring

--- src/test/java/session03/ClazzTest.java
+++ src/test/java/session03/ClazzTest.java
@@ -21,6 +21,8 @@ public class ClazzTest {
 		
 		Teacher teacher1 = ctx.getBean("teacher1", Teacher.class);
 		System.out.println(teacher1);
+		Teacher teacher2 = ctx.getBean("teacher2", Teacher.class);
+		System.out.println(teacher2);
 	}
 
 }
