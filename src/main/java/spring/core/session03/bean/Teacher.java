94a9efa8f4da825b8305534b7e8133ffad8f9930
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 14:03:17 +0800

    spring

---
+++ src/main/java/spring/core/session03/bean/Teacher.java
@@ -0,0 +1,51 @@
+package spring.core.session03.bean;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Teacher {
+	private Integer id;  // 講師編號
+	private String name; // 講師姓名
+	private Set<Student> students; // 學生列表
+	private List<String> subjects; // 授課列表
+	private Map<String, Integer> salary; // 薪資組合
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Set<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+	public List<String> getSubjects() {
+		return subjects;
+	}
+	public void setSubjects(List<String> subjects) {
+		this.subjects = subjects;
+	}
+	public Map<String, Integer> getSalary() {
+		return salary;
+	}
+	public void setSalary(Map<String, Integer> salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Teacher [id=" + id + ", name=" + name + ", students=" + students + ", subjects=" + subjects
+				+ ", salary=" + salary + "]";
+	}
+	
+	
+}
