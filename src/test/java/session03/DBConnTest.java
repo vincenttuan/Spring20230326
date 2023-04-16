b60f59aa549e1d23da450f9b2bb3dd1b06a0c17e
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 15:59:02 +0800

    spring

--- src/test/java/session03/DBConnTest.java
+++ src/test/java/session03/DBConnTest.java
@@ -1,13 +1,16 @@
 package session03;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.core.session03.bean.DBConn;
+import spring.core.session03.java_conf.AppConfig;
 
 public class DBConnTest {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		//ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 		
 		DBConn dbConn = ctx.getBean("dbConn", DBConn.class);
 		dbConn.create();
