e7ff8c4f83fd027cf3f17e0449be2dbe9b0f26c2
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 16:43:51 +0800

    spring

---
+++ src/test/java/session03/StudentTest.java
@@ -0,0 +1,20 @@
+package session03;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session03.bean.Student;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		List<Student> students = ctx.getBean("students", List.class);
+		System.out.println(students);
+		// 請印出每一個學生各修了多少學分 ?
+		
+	}
+
+}
