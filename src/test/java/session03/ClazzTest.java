dc71114843567b4d3d8632b601629613369070df
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-16 13:56:51 +0800

    spring

--- src/test/java/session03/ClazzTest.java
+++ src/test/java/session03/ClazzTest.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.core.session03.bean.Clazz;
+import spring.core.session03.bean.Student;
 
 public class ClazzTest {
 
@@ -11,6 +12,12 @@ public class ClazzTest {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
 		Clazz clazz1 = ctx.getBean("clazz1", Clazz.class);
 		System.out.println(clazz1);
+		
+		Student student1 = ctx.getBean("student1", Student.class);
+		Student student2 = ctx.getBean("student2", Student.class);
+		System.out.println(student1);
+		System.out.println(student2);
+		
 	}
 
 }
