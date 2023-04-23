010788f5e2ff7e218be47f23e22f344969bdf9ac
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-23 14:38:57 +0800

    spring

---
+++ src/main/java/spring/core/session03/mvc/AppConfig.java
@@ -0,0 +1,12 @@
+package spring.core.session03.mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "spring.core.session03.mvc")
+public class AppConfig {
+	
+	// 可以添加其他配置...
+	
+}
