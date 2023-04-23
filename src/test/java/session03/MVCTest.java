26e7b5a0c335eb7c83606eb692c4f3fe0b585b5f
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-23 14:32:22 +0800

    spring

--- src/test/java/session03/MVCTest.java
+++ src/test/java/session03/MVCTest.java
@@ -1,8 +1,11 @@
 package session03;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.core.session03.mvc.controller.UserController;
 import spring.core.session03.mvc.model.User;
 
 public class MVCTest {
@@ -16,6 +19,16 @@ public class MVCTest {
 		System.out.println(user.getSubjects().size());
 		System.out.println(user.getScores().size());
 		System.out.println(user.getHobbies().size());
+		System.out.println("------------------------------------------------------------------");
+		// 透過 UserController 新增 User
+		UserController userController = ctx.getBean("userController", UserController.class);
+		userController.append(user);
+		userController.append(user);
+		userController.append(user);
+		// 查詢所有 users 的資料
+		List<User> users = userController.queryAll();
+		System.out.println(users);
+		System.out.println("資料筆數: " + users.size());
 	}
 
 }
