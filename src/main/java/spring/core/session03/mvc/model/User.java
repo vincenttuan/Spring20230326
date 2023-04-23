c168e70f54a5176c24b8461971732f2741b68044
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-23 13:14:26 +0800

    spring

---
+++ src/main/java/spring/core/session03/mvc/model/User.java
@@ -0,0 +1,81 @@
+package spring.core.session03.mvc.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	
+	private String username; // 姓名
+	
+	private Integer age; // 年齡
+	
+	private String[] nickname; // 暱稱
+	
+	private Set<String> subjects; // 科目
+	
+	private List<Integer> scores; // 成績
+	
+	private Map<String, String> hobbies; // 興趣
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String[] getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String[] nickname) {
+		this.nickname = nickname;
+	}
+
+	public Set<String> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(Set<String> subjects) {
+		this.subjects = subjects;
+	}
+
+	public List<Integer> getScores() {
+		return scores;
+	}
+
+	public void setScores(List<Integer> scores) {
+		this.scores = scores;
+	}
+
+	public Map<String, String> getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(Map<String, String> hobbies) {
+		this.hobbies = hobbies;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", age=" + age + ", nickname=" + Arrays.toString(nickname) + ", subjects="
+				+ subjects + ", scores=" + scores + ", hobbies=" + hobbies + "]";
+	}
+	
+	
+	
+}
