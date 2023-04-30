3c2219bb7a004a0a6a331ee52a34bb37b7e8b9e7
vincenttuan <vincentjava@yahoo.com.tw>
2023-04-30 16:34:26 +0800

    spring

---
+++ src/main/java/spring/core/session05/aop_lab/BackSinger.java
@@ -0,0 +1,10 @@
+package spring.core.session05.aop_lab;
+
+public class BackSinger implements Singer {
+
+	@Override
+	public void sing() {
+		System.out.println("唱歌：啦啦啦~~~");
+	}
+
+}
---
+++ src/main/java/spring/core/session05/aop_lab/Singer.java
@@ -0,0 +1,7 @@
+package spring.core.session05.aop_lab;
+
+// 歌者
+public interface Singer {
+	// 唱歌
+	public void sing();
+}
