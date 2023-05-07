08b8d2f395a9fbc13b03d3def5b511bc66f86fef
vincenttuan <vincentjava@yahoo.com.tw>
2023-05-07 14:06:52 +0800

    spring

---
+++ src/test/java/session06/DBConnTest.java
@@ -0,0 +1,23 @@
+package session06;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class DBConnTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+		ComboPooledDataSource cp = ctx.getBean("dataSource", ComboPooledDataSource.class);
+		try {
+			System.out.println(cp.getConnection()); // 資料庫連線
+			System.out.println("連線成功");
+		} catch (Exception e) {
+			System.out.println("連線失敗");
+			e.printStackTrace();
+		}
+		
+	}
+
+}
