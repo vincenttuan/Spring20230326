01b609bf6b52b4acb56c3ca850f54c9f2e794f6d
vincenttuan <vincentjava@yahoo.com.tw>
2023-05-07 16:52:13 +0800

    spring

--- src/main/java/spring/core/session06/controller/EmpController.java
+++ src/main/java/spring/core/session06/controller/EmpController.java
@@ -1,5 +1,26 @@
 package spring.core.session06.controller;
 
-public class EmpController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import spring.core.session06.jdbc.entity.Emp;
+import spring.core.session06.service.EmpService;
+
+@Controller
+public class EmpController {
+	
+	@Autowired
+	private EmpService empService;
+	
+	public List<Emp> queryEmps() {
+		return empService.findAllEmps();
+	}
+	
+	public Integer getMaxOfAge() {
+		return empService.getMaxOfAge();
+	}
+	
+	
 }
