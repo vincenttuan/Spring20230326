b7113537707e7b9a755d9ee7afb01cf5ff637b94
vincenttuan <vincentjava@yahoo.com.tw>
2023-05-07 16:42:32 +0800

    spring

--- src/main/java/spring/core/session06/service/EmpService.java
+++ src/main/java/spring/core/session06/service/EmpService.java
@@ -1,5 +1,12 @@
 package spring.core.session06.service;
 
-public interface EmpService {
+import java.util.List;
+import java.util.Optional;
 
+import spring.core.session06.jdbc.entity.Emp;
+
+public interface EmpService {
+	public List<Emp> findAllEmps();
+	// ...
+	public Integer getMaxOfAge(); // 取得員工的最大年齡
 }
--- src/main/java/spring/core/session06/service/EmpServiceImpl.java
+++ src/main/java/spring/core/session06/service/EmpServiceImpl.java
@@ -1,5 +1,29 @@
 package spring.core.session06.service;
 
-public class EmpServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import spring.core.session06.jdbc.dao.EmpDao;
+import spring.core.session06.jdbc.entity.Emp;
+
+@Service
+public class EmpServiceImpl implements EmpService {
+	
+	@Autowired
+	private EmpDao empDao;
+	
+	@Override
+	public List<Emp> findAllEmps() {
+		return empDao.queryEmps();
+	}
+
+	@Override
+	public Integer getMaxOfAge() {
+		List<Emp> emps = findAllEmps();
+		return emps.stream().mapToInt(Emp::getAge).findAny().getAsInt();
+	}
 
 }
