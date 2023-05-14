53665c7b14b90be5c30542fc4e825d299ad864ca
vincenttuan <vincentjava@yahoo.com.tw>
2023-05-14 14:14:04 +0800

    tx

--- src/main/java/spring/core/session07/tx/service/BookManyServiceImpl.java
+++ src/main/java/spring/core/session07/tx/service/BookManyServiceImpl.java
@@ -1,7 +1,9 @@
 package spring.core.session07.tx.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookManyServiceImpl implements BookManyService {
 	
 	@Autowired
